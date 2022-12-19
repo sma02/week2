09136869d35a06690c21ed383ccf22733fff8492
reroute <sma02022@outlook.com>
2022-12-19 23:11:26 +0500

    added pf week2 files

---
+++ creativity.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"       ____                   ____    "<<endl;
+cout<<"      -    -                 -    -   "<<endl;
+cout<<"     _________             _________  "<<endl;
+cout<<"    |         |           |         | "<<endl;
+cout<<"    |    ^    |           |    ^    | "<<endl;
+cout<<"    |_________|           |_________| "<<endl;
+cout<<"                                      "<<endl;
+cout<<"                                      "<<endl;
+cout<<"          _                   _       "<<endl;
+cout<<"           \\_________________/       "<<endl;
+}
